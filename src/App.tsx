c7bbd3303283538b776863fae69bdfda81f8fc5f
도오성 <dos@dooseong-ui-MacBook-Pro.local>
2019-12-10 22:46:52 +0900

    진도좀 나가자

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './resources/css/index.css';
 import BlogWritePage from './pages/BlogWritePage';
+import MainPage from './pages/MainPage';
+import PrdListPage from './pages/PrdListPage';
 const App: React.FC = () => {
  
   return (
       <div id="app">
         <Router>
-          {/* 
-          <header>
-            <Link to="/">Home</Link>
-            <Link to="/">글쓰기</Link>
-          </header>
-          */}
           <Switch>
-            <Route path="/" exact component={BlogWritePage} />
+            <Route path="/" exact component={MainPage} />
+            <Route path="/prod" exact component={PrdListPage} />
+            <Route path="/blog" exact component={BlogWritePage} />
           </Switch>
         </Router>
       </div>
---
+++ src/pages/PrdListPage.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ProdGrid from '../components/ProdGrid';
+
+class PrdListPage extends React.Component {
+    render = (): JSX.Element => {
+        return (
+            <div className="page-container">
+                <div className="page-header">청송목조각 나무조각들</div>
+                <div className="page-content">
+                    <div className="slide-area">
+                        <img src="http://demoapus-wp.com/maison/wp-content/uploads/2017/07/Layer-24.png" alt="aaa"/>
+                    </div>
+                    <div className="prod-list-wrap">
+                        <div className="tag-area">
+                            <div className="tag-item"># 태그1</div>
+                            <div className="tag-item"># 태그2</div>
+                            <div className="tag-item"># 태그3</div>
+                            <div className="tag-item"># 태그4</div>
+                            <div className="tag-item"># 태그5</div>
+                        </div>
+                        <ProdGrid/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default PrdListPage;
