17e5036a6a5d2ab8cde325fb5a559017d558c918
unknown <noblesse721@gmail.com>
2020-02-08 19:35:59 +0900

    #2 태그 화면 임시시안

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -4,7 +4,16 @@ interface Props {}
 
 class Tag extends React.Component<Props> {
     render = (): JSX.Element => {
-        const arrTitle  = ["자바스크립트", "자바", "파이썬", "스프링프레임워크", "전자정부프레임워크"];
+        const arrTitle  = ["자바스크립트", "자바", "파이썬", "스프링프레임워크", "전자정부프레임워크",
+        "자바스크립트", "자바", "파이썬", "스프링프레임워크", "전자정부프레임워크",
+        "자바스크립트", "자바", "파이썬", "스프링프레임워크", "전자정부프레임워크",
+        "자바스크립트", "자바", "파이썬", "스프링프레임워크", "전자정부프레임워크",
+        "자바스크립트", "자바", "파이썬", "스프링프레임워크", "전자정부프레임워크",
+        "자바스크립트", "자바", "파이썬", "스프링프레임워크", "전자정부프레임워크",
+        "자바스크립트", "자바", "파이썬", "스프링프레임워크", "전자정부프레임워크",
+        "자바스크립트", "자바", "파이썬", "스프링프레임워크", "전자정부프레임워크",
+        "자바스크립트", "자바", "파이썬", "스프링프레임워크", "전자정부프레임워크"
+        ];
         const titleList = arrTitle.map(
             (title, i) => (
                 <div key={i} className="tag-item-wrap">
@@ -18,12 +27,14 @@ class Tag extends React.Component<Props> {
         );
 
         return (
-            <div className="tag-list-wrap">
-               <div className="search-bar-wrap">
-                   <i className="fas fa-search"></i>
-                   <input type="text" placeholder="검색할 태그명을 입력하세요."/>
+            <div className="tag-container">
+                <div className="search-bar-wrap">
+                    <i className="fas fa-search"></i>
+                    <input type="text" placeholder="검색할 태그명을 입력하세요."/>
                 </div>
-               {titleList}
+                <div className="tag-list-wrap">
+                    {titleList}
+                </div>
             </div>
         )
     }
