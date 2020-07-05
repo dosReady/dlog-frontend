35fac1ce57ff9e2b7220f15dc80d7c14876aa894
도오성 <dos@dooseong-ui-MacBook-Pro.local>
2020-07-05 11:38:21 +0900

    adsd

--- src/org/dlog/comn/LoginSrvc.tsx
+++ src/org/dlog/comn/LoginSrvc.tsx
@@ -7,7 +7,6 @@ class LoginSrvc {
     
     public setLocalStorage(param:User):void {
         if(param !== null) {
-            window.localStorage.removeItem(STORENAME);
             window.localStorage.setItem(STORENAME, JSON.stringify(param))
             sec.defaults.headers.common = { 'Authorization': `Bearer ${param.AccessToken}` };
         }
