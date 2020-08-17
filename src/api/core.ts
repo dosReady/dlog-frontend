e8343d311e144b79c7afe969a5cc8272557f4175
도오성 <dos@dooseong-ui-MacBook-Pro.local>
2020-08-17 16:25:11 +0900

    sad

--- src/api/core.ts
+++ src/api/core.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const api =  axios.create(
     {
-        baseURL: "http://127.0.0.1:8080",
-        //baseURL: "https://api.dveloper.me/",
+        //baseURL: "http://127.0.0.1:8080",
+        baseURL: "https://api.dveloper.me/",
         headers: {
             "content-type": "application/json"
         },
